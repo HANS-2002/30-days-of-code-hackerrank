18903329fe0b9cc5232c8bc017d0621642b03a6d
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2020-05-09 13:46:22 +0000

    Create Day 17: More Exceptions.cpp

---
+++ Day 17: More Exceptions.cpp
@@ -0,0 +1,15 @@
+
+
+//Write your code here
+class Calculator
+{
+    public:
+    static int power(const int n_, const int p_) {
+        //Write your code here
+        if ( n_ < 0 || p_ < 0 ) { throw std::invalid_argument("n and p should be non-negative");}
+        return pow(n_, p_);
+    }
+    
+};
+
+
